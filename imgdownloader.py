1b0b7f445a88ee1b627e41ff4404d2249170f69a
chen yang <kevinyang9708@gmail.com>
2020-01-26 10:55:12 -0500

    ...

--- imgdownloader.py
+++ imgdownloader.py
@@ -15,13 +15,14 @@ def download_img(img_url, imgname):
         return "failed"
 
 if __name__ == '__main__':
-    file = 'C:\\Users\\28015\\Desktop\\商品库存列表导出(1-500).xlsx'
+    file = 'E:\\商品图片excel\\商品库存列表(1-8095).xlsx'
     wb = xlrd.open_workbook(filename=file)  # 打开文件
     sheet1 = wb.sheet_by_index(0)  # 通过索引获取表格
-    for i in range(266):
+    rows = sheet1.nrows - 1# 得到数据的总行数
+    for i in range(rows + 1):
         # 下载要的图片
-        imgname = sheet1.cell(i + 1,0).value
-        img_url = sheet1.cell(i + 1,1).value
-        print(download_img(img_url, imgname))
+        imgname = sheet1.cell(i, 1).value
+        img_url = sheet1.cell(i, 4).value
+        print("剩余: {} 张图片".format(i))
 
 
