481514c94c6c8328f4feacbe065ea246e3eb9058
chen <kevinyang9708@gmail.com>
2020-04-24 15:20:06 -0400

    完成自动下载功能

--- imgdownloader.py
+++ imgdownloader.py
@@ -1,34 +1,52 @@
 # coding: utf8
 import xlrd
 import urllib.request
+import os
 
-
-def download_img(img_url, imgname):
+def download_img(img_url, imgID, imgdir_path):
     request = urllib.request.Request(img_url)
     try:
         response = urllib.request.urlopen(request)
-        filename = "E:\\商品图片\\" + imgname + ".jpg"
-        if (response.getcode() == 200):
+        # filename = "E:\\商品图片\\" + imgID + ".jpg"
+        filename = imgdir_path + imgID + ".jpg"
+        if response.getcode() == 200:
             with open(filename, "wb") as f:
                 f.write(response.read())  # 将内容写入图片
-            return filename
+            return "成功下载: " + filename
     except:
-        return "failed"
+        return "失败，未下载到图片: " + imgID
 
 
 if __name__ == '__main__':
-    file = 'E:\\商品图片excel\\商品库存列表(1-8095).xlsx'
-    wb = xlrd.open_workbook(filename=file)  # 打开文件
-    sheet1 = wb.sheet_by_index(0)  # 通过索引获取表格
-    rows = sheet1.nrows - 1  # 得到数据的总行数
-    for i in range(rows):
-        # 下载要的图片
-        imgname = sheet1.cell(i + 735, 1).value
-        img_url = sheet1.cell(i + 735, 4).value
-        if (img_url == ""):
-            print(imgname + '他妈的没有链接')
-            continue
-        print(download_img(img_url, imgname))
-        print("剩余: {} 张图片".format(rows - 735 - i))
+    ### 参数设置 ###
+    # file = 'E:\\商品图片excel\\商品库存列表(1-8095).xlsx'
+    xlsx_path = "xlsx/to8664.xlsx"
+    imgdir_path = "/Users/chen/Desktop/下载测试/"
+    sheetIndex = 1
 
+    ###############
+    wb = xlrd.open_workbook(filename=xlsx_path)  # 打开xlsx文件
+    sheet1 = wb.sheet_by_index(sheetIndex)  # 通过索引获取表格
+    totalRows = sheet1.nrows  # 得到数据的总行数
 
+    # 读取图片目录下所有的文件名
+    allFile = os.listdir(imgdir_path)
+    for i in range(len(allFile)):  # 删除 ".jpg" 只保留编号
+        allFile[i] = allFile[i].split(".", 1)[0]
+    # 读取xlsx下所有ID和URL，存为字典
+    allIDandUrl = {}
+    for i in range(totalRows - 1):
+        allIDandUrl[sheet1.cell(i + 1, 2).value] = sheet1.cell(i + 1, 5).value  # ID: URL
+    # 读取xlsx下所有ID
+    allID = [0] * (totalRows - 1)
+    for i in range(totalRows - 1):
+        allID[i] = sheet1.cell(i + 1, 2).value
+    # 求差集，在xlsx中但不在图片目录中
+    newID = list(set(allID).difference(set(allFile)))
+
+    for i in range(len(newID)):
+        # 下载新增的图片
+        imgID = newID[i]
+        img_url = allIDandUrl.get(imgID)
+        print(download_img(img_url, imgID, imgdir_path))
+        print("剩余: {} 张图片".format(len(newID) - (i+1)))
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import xlrd
+import urllib.request
 
 from datetime import date, datetime
 
@@ -10,13 +11,12 @@ wb = xlrd.open_workbook(filename=file)  # 打开文件
 # print(wb.sheet_names())#获取所有表格名字
 sheet1 = wb.sheet_by_index(1)  # 通过索引获取表格
 
-print(sheet1.cell(737, 4).value)
-print(sheet1.cell(737, 4).value == "")
-print(type(sheet1.cell(737, 4).value))
-# 想要获取第八行的商品名称
-row = 8
+print(sheet1.cell(7, 5).value)
+print(sheet1.cell(7, 5).value == "")
+
+row = 8  # 想要获取第八行的商品名称
 col = 1  # B列
-print(sheet1.cell(row - 1, col).value)  # 获取表格里的内容，三种方式
+print(sheet1.cell(row - 1, col).value)  # 获取表格里的内容
 print(sheet1.nrows)
 
 import os
@@ -24,5 +24,27 @@ allFile = os.listdir("/Users/chen/Desktop/商品图片")
 for i in range(len(allFile)):
     allFile[i] = allFile[i].split(".", 1)[0]
 
-for i in range(5):
-    print(allFile[i])
+mylist = [[0] * 2 for i in range(sheet1.nrows - 1)]
+for i in range(sheet1.nrows - 1):
+    mylist[i][0] = sheet1.cell(i + 1, 2).value
+    mylist[i][1] = sheet1.cell(i + 1, 5).value
+
+id = mylist[len(mylist)-1][0]
+url =mylist[len(mylist)-1][1]
+
+
+allIDandUrl = {}
+for i in range(sheet1.nrows - 1):
+    allIDandUrl[sheet1.cell(i + 1, 2).value] = sheet1.cell(i + 1, 5).value  # ID: URL
+
+ans = allIDandUrl.get("ED831164KC1")
+print(ans)
+#求差集，在B中但不在A中
+# retD = list(set(mylist).difference(set(allFile)))
+# print(len(allFile))
+# print(len(retD))
+
+
+
+
+
