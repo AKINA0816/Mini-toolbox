ca4894a020cf63e9451cfa9c942df356a2616d46
chen <kevinyang9708@gmail.com>
2020-04-23 23:26:29 -0400

    搞到一半

--- imgdownloader.py
+++ imgdownloader.py
@@ -2,31 +2,33 @@
 import xlrd
 import urllib.request
 
+
 def download_img(img_url, imgname):
     request = urllib.request.Request(img_url)
     try:
         response = urllib.request.urlopen(request)
-        filename = "E:\\商品图片\\"+ imgname + ".jpg"
+        filename = "E:\\商品图片\\" + imgname + ".jpg"
         if (response.getcode() == 200):
             with open(filename, "wb") as f:
-                f.write(response.read()) # 将内容写入图片
+                f.write(response.read())  # 将内容写入图片
             return filename
     except:
         return "failed"
+
 
 if __name__ == '__main__':
     file = 'E:\\商品图片excel\\商品库存列表(1-8095).xlsx'
     wb = xlrd.open_workbook(filename=file)  # 打开文件
     sheet1 = wb.sheet_by_index(0)  # 通过索引获取表格
-    rows = sheet1.nrows - 1# 得到数据的总行数
+    rows = sheet1.nrows - 1  # 得到数据的总行数
     for i in range(rows):
         # 下载要的图片
         imgname = sheet1.cell(i + 735, 1).value
         img_url = sheet1.cell(i + 735, 4).value
-        if(img_url == ""):
+        if (img_url == ""):
             print(imgname + '他妈的没有链接')
             continue
-        print(download_img(img_url,imgname))
-        print("剩余: {} 张图片".format(rows-735 - i))
+        print(download_img(img_url, imgname))
+        print("剩余: {} 张图片".format(rows - 735 - i))
 
 
--- test.py
+++ test.py
@@ -1,25 +1,28 @@
 import xlrd
 
-from datetime import date,datetime
+from datetime import date, datetime
 
-
-
-file = 'E:\\商品图片excel\\商品库存列表(1-8095).xlsx'
+file = 'xlsx/to8664.xlsx'
 
 print(file)
 
-wb = xlrd.open_workbook(filename=file)#打开文件
+wb = xlrd.open_workbook(filename=file)  # 打开文件
 # print(wb.sheet_names())#获取所有表格名字
-sheet1 = wb.sheet_by_index(0)#通过索引获取表格
+sheet1 = wb.sheet_by_index(1)  # 通过索引获取表格
 
-print(sheet1.cell(737,4).value)
-print(sheet1.cell(737,4).value == "")
-print(type(sheet1.cell(737,4).value))
-print(sheet1.cell(266,1).value)#获取表格里的内容，三种方式
+print(sheet1.cell(737, 4).value)
+print(sheet1.cell(737, 4).value == "")
+print(type(sheet1.cell(737, 4).value))
+# 想要获取第八行的商品名称
+row = 8
+col = 1  # B列
+print(sheet1.cell(row - 1, col).value)  # 获取表格里的内容，三种方式
 print(sheet1.nrows)
 
+import os
+allFile = os.listdir("/Users/chen/Desktop/商品图片")
+for i in range(len(allFile)):
+    allFile[i] = allFile[i].split(".", 1)[0]
 
-for i in range(10):
-    if(i==5):
-        continue
-    print(i)
+for i in range(5):
+    print(allFile[i])
